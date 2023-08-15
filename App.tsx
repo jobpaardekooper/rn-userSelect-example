9d839ea81f22ce3eaa7d571bf4ea15f4797fac4e
Job Paardekooper <jobpaardekooper@gmail.com>
2023-08-15 13:48:41 +0200

    first commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text style={{ userSelect: 'auto' }}>Text element is selectable</Text> 
+    </View>
+  );
+}
+
+export default App;
